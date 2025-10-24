7fff0bfdd0f0e70aa3edf7307060c559695c5071
Tevfik Gülep <tevfikgulep@oxigen.team>
2025-10-24 12:56:14 +0000

    Stop tracking firebase config file

--- src/firebase/config.ts
+++
@@ -1,9 +0,0 @@
-
-export const firebaseConfig = {
-  "projectId": "rank-tracker-pro-3000",
-  "appId": "1:516533369689:web:a8fd94385ec22a829d2a48",
-  "apiKey": "YENİ_API_ANAHTARINIZI_BURAYA_YAPIŞTIRIN",
-  "authDomain": "rank-tracker-pro-3000.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "516533369689"
-};
